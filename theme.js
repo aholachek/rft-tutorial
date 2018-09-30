86475d93dc129009cb8900a692804602a9db1572
Alex Holachek <aholachek@gmail.com>
2018-09-30 18:47:51 -0400

    minor cleanup

--- theme.js
+++ theme.js
@@ -2,10 +2,19 @@ import theme from "mdx-deck/themes"
 
 export default {
   ...theme,
-
+font: '"Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
   colors: {
     text: "#ffd379",
     background: "#282C34",
     link: "#79B4F1"
-  }
+  },
+  h3: {
+    fontWeight: 'normal'
+  },
+  h4: {
+    fontWeight: 'normal'
+  },
+  h5: {
+    fontWeight: 'normal'
+  },
 }
